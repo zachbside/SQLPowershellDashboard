85cb0dd3ce1a8a10e66f8adeb53005e15b8d7982
zachbside <53321834+zachbside@users.noreply.github.com>
2019-07-30 12:44:10 -0400

    Added Violation Logic

--- GatherSP.sql
+++ GatherSP.sql
@@ -1,75 +1,120 @@
-USE [test]
-GO
+/****** Object:  StoredProcedure [dbo].[GatherMetrics]    Script Date: 7/30/2019 12:12:58 PM ******/ 
+SET ansi_nulls ON 
 
-/****** Object:  StoredProcedure [dbo].[GatherMetrics]    Script Date: 7/29/2019 12:19:45 AM ******/
-SET ANSI_NULLS ON
-GO
+go 
 
-SET QUOTED_IDENTIFIER ON
-GO
+SET quoted_identifier ON 
 
+go 
 
-
--- ============================================= 
--- Author:    Zach Burnside 
--- Create date: 7/25/2019 
--- Description:  Stored procedure used to gather metrics on the local SQL server 
--- ============================================= 
-CREATE PROCEDURE [dbo].[GatherMetrics] 
+-- =============================================       
+-- Author:    Zach Burnside       
+-- Create date: 7/25/2019       
+-- Description:  Stored procedure used to gather metrics on the local SQL server       
+-- =============================================       
+ALTER PROCEDURE [dbo].[Gathermetrics] 
 AS 
   BEGIN 
       SET nocount ON; 
-	  
-      DECLARE @CurrentMemory float 
-	  DECLARE @MaxMemory float
-      DECLARE @BatchesPerSec INT 
-      DECLARE @AVG INT 
+
+      DECLARE @CurrentMemory FLOAT 
+      DECLARE @MaxMemory FLOAT 
+      DECLARE @UserConnections INT 
       DECLARE @ViolationBit BIT = 0 
-      --Get the current memory as a percentage. current memory / max server memory allocated
+      DECLARE @Threshold INT 
+      DECLARE @NumOfMetrics INT 
+      DECLARE @ValueOfMetric INT 
+
+      --Get the current memory as a percentage. current memory / max server memory allocated    
       SET @CurrentMemory=(SELECT ( physical_memory_in_use_kb / 1024 ) AS 
                                  Memory_GB 
                           FROM   sys.dm_os_process_memory) 
-      SET @MaxMemory = convert(float,(SELECT value
-					   FROM sys.configurations  
-                       WHERE [name] = 'max server memory (MB)'))
-      SET @CurrentMemory = (@CurrentMemory/@MaxMemory)*100
+      SET @MaxMemory = CONVERT(FLOAT, (SELECT value 
+                                       FROM   sys.configurations 
+                                       WHERE  [name] = 'max server memory (MB)') 
+                       ) 
+      SET @CurrentMemory = ( @CurrentMemory / @MaxMemory ) * 100 
+      --set variable to the threshold that is written in the metrics table   
+      SET @Threshold = (SELECT threshold 
+                        FROM   dbo.metrics 
+                        WHERE  metricname = 'Memory') 
 
-      --if memory used is above 85% send an alert 
-      IF @CurrentMemory > 85 
-		BEGIN
-        SET @ViolationBit=1 
-		END
-      
-      --insert the date, metric name(Memory), metric value(Memory % used), and the violation bit 
-      INSERT INTO  test.dbo.CurrentMetric 
+      --if in violation of of the threshold, set violation bit to 1   
+      IF @CurrentMemory > @Threshold 
+        BEGIN 
+            SET @ViolationBit=1 
+        END 
+
+      --insert the metricID, metric value(Memory % used), and the violation bit into the CurrentMetric table   
+      INSERT INTO test.dbo.currentmetric 
       VALUES      ( 2, 
                     @CurrentMemory, 
                     @ViolationBit ) 
 
-      --check the number of batches per second 
-      SET @BatchesPerSec = (SELECT cntr_value 
-                            FROM   sys.dm_os_performance_counters 
-                            WHERE  counter_name LIKE 'Batch Requests/sec%') 
-							
-      --reset violation bit 
+      --check the number of users connected       
+      SET @UserConnections = (SELECT Count(*) 
+                              FROM   sys.dm_exec_sessions 
+                              WHERE  is_user_process = 1) 
+      --reset violation bit       
       SET @ViolationBit = 0 
+      --check if the number of user connections is over the threshold      
+      SET @Threshold = (SELECT threshold 
+                        FROM   dbo.metrics 
+                        WHERE  metricname = 'UserConnections') 
 
-      --check if the number of batches per second is higher than 8000 
-      IF @BatchesPerSec > 8000
+      --if this value is above the threshold, set the violation bit   
+      IF @UserConnections > @Threshold 
         SET @ViolationBit=1 
 
-      --insert the date, metric name(batches per second), metric value(batches per second), and the violation bit
-      INSERT INTO  test.dbo.CurrentMetric 
+      --insert the metric name(connections), metric value(number of connections), and the violation bit into the CurrentMetric table
+      INSERT INTO test.dbo.currentmetric 
       VALUES      ( 3, 
-                    @BatchesPerSec, 
-                    @ViolationBit )
+                    @UserConnections, 
+                    @ViolationBit ) 
 
-	  insert into dbo.Metrics_Repo 
-		select MetricID, Value, getdate() from test.dbo.CurrentMetric
-    
-	  
+      --Log the values from CurrentMetric into the Metric_Repo history table   
+      INSERT INTO dbo.metrics_repo 
+      SELECT metricid, 
+             value, 
+             Getdate() 
+      FROM   dbo.currentmetric 
+
+      --Determine the number of metrics that need to be accounted for   
+      SET @NumOfMetrics = (SELECT Count(*) 
+                           FROM   dbo.currentmetric) 
+
+      --loop through the metrics, checking for violations in CurrentMetric   
+      WHILE ( @NumOfMetrics > 0 ) 
+        BEGIN 
+            SET @ValueOfMetric = (SELECT value 
+                                  FROM   currentmetric 
+                                  WHERE  metricid = @NumOfMetrics) 
+
+            --check if the violation bit is true per metric  
+            IF ( (SELECT violationbit 
+                  FROM   currentmetric 
+                  WHERE  metricid = @NumOfMetrics) = 1 ) 
+              BEGIN 
+                  --check if the metric is in the violation table 
+                  IF NOT EXISTS (SELECT * 
+                                 FROM   violations 
+                                 WHERE  metricid = @NumOfMetrics) 
+                    BEGIN 
+                        --insert the violation information into the Violations table  
+                        INSERT INTO dbo.violations 
+                        VALUES      (@NumOfMetrics, 
+                                     @ValueOfMetric, 
+                                     Getdate()) 
+
+                        RETURN 
+                    END 
+              END 
+            ELSE 
+              --if metric is not violating (or never was) remove it from the violations table   
+              DELETE FROM violations 
+              WHERE  metricid = @NumOfMetrics 
+
+            --increment counter  
+            SET @NumOfMetrics = @NumOfMetrics - 1 
+        END 
   END 
-
-GO
-
-
