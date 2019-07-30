e7c85369656125eca1500d1b0ab994c0bd8e9f44
zachbside <53321834+zachbside@users.noreply.github.com>
2019-07-30 12:41:53 -0400

    Added Threshold to Metrics and added base rows

--- CreateTables.sql
+++ CreateTables.sql
@@ -1,22 +1,22 @@
-
 create table Contacts (
 	ContactID int NOT NULL Primary Key,
 	ContactName varchar(50)
-	)
+)
 
 create table Metrics (
 	MetricID int NOT NULL Primary Key,
 	MetricName varchar(50) NOT NULL,
-	ContactID int NOT NULL
+	ContactID int NOT NULL,
+	Threshold int NOT NULL
 	FOREIGN KEY (ContactID) REFERENCES Contacts(ContactID)
-	)
+)
 
 create table Metrics_Repo (
 	MetricID int NOT NULL,
 	Value int NOT NULL,
 	Date datetime NOT NULL
 	FOREIGN KEY (MetricID) REFERENCES Metrics(MetricID)
-	)
+)
 
 CREATE TABLE [dbo].[Violations](
 	[MetricID] int NOT NULL PRIMARY KEY,
@@ -27,7 +27,18 @@ CREATE TABLE [dbo].[Violations](
 
 CREATE TABLE dbo.CurrentMetric (
 	[MetricID] int NOT NULL PRIMARY KEY,
-	[VALUE] int NOT NULL
+	[VALUE] int NOT NULL,
 	ViolationBit bit NOT NULL
 	FOREIGN KEY (MetricID) REFERENCES Metrics(MetricID)
-	)
+)
+  --insert base contacts
+  insert into contacts values (1, 'dba')
+  insert into contacts values (2, 'sysadmin')
+  
+  --insert base metrics
+  insert into metrics values (1,'CPU',2, 50)
+  insert into metrics values (2,'Memory',1, 85)
+  insert into metrics values (3,'UserConnections',1,15)
+  
+  --allow user to view the dmvs
+  grant view server state to tester
